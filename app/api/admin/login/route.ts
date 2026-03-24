7ba52841c6473f880e712d7f7aece3cc34cdbeed
Amwighio <manan7866@users.noreply.github.com>
2026-03-24 19:03:49 +0500

    user profile iamge setting solved

--- app/api/admin/login/route.ts
+++ app/api/admin/login/route.ts
@@ -79,6 +79,7 @@ export async function POST(request: Request) {
       email: user.email,
       full_name: user.full_name,
       role: user.role,
+      avatar_url : user.avatar_url,
     }), {
       httpOnly: false,
       secure: process.env.NODE_ENV === 'production',
